d839114ccca4187f66c79cb4cf3b1d9c6208031c
caiocrnyimi <caiocrnyimi@gmail.com>
2025-09-23 22:29:17 -0300

    funcionando

--- src/main/java/com/fiap/mottu_patio/dto/MotoRequest.java
+++ src/main/java/com/fiap/mottu_patio/dto/MotoRequest.java
@@ -6,7 +6,6 @@ import lombok.Data;
 public class MotoRequest {
     private String placa;
     private String modelo;
-    private String cor;
     private Integer ano;
     private Integer quilometragem;
     private Long patioId;
